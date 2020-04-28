e0c41179a3793377241ff645585a64d7f0c1321d
Richard White <w@rwhite.no>
2020-04-28 20:26:47 +0200

    source /etc/environmensource /etc/environmentt

--- bin/dbase_push.sh
+++ bin/dbase_push.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source $WORKSPACE/bin/R_VERSION.sh
+source /etc/environment
+
+sudo podman login docker.io -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 sudo podman push fhix/dbase:$RVERSION
--- bin/dr_push.sh
+++ bin/dr_push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source $WORKSPACE/bin/R_VERSION.sh
+source /etc/environment
 
-sudo podman login docker.io
+sudo podman login docker.io -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 sudo podman push fhix/dr:$RVERSION
 
 sudo podman tag fhix/dr:$RVERSION fhix/dr:latest
