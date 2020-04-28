d8fcd4951f441ff394fe7542c3148bbdc1186ca9
Richard White <w@rwhite.no>
2020-04-28 20:47:18 +0200

    more docker

--- bin/dr_push.sh
+++ bin/dr_push.sh
@@ -3,7 +3,7 @@ source $WORKSPACE/bin/R_VERSION.sh
 source /etc/environment
 
 sudo podman login docker.io -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-sudo podman push fhix/dr:$RVERSION
+sudo podman push docker.io/fhix/dr:$RVERSION
 
 sudo podman tag fhix/dr:$RVERSION fhix/dr:latest
-sudo podman push fhix/dr:latest
+sudo podman push docker.io/fhix/dr:latest
