5a29bee6cb6a0867a60f56bdccac87172784b7be
calvin chiang <calvin.chiang@gmail.com>
2021-01-21 11:45:18 +0100

    finalised addition of cran_checkpoint env var

--- bin/dbase_build.sh
+++ bin/dbase_build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 export NOCACHE=--no-cache
 source $WORKSPACE/bin/R_VERSION.sh
+source $WORKSPACE/bin/CRAN_CHECKPOINT.sh
 
 sudo podman rmi fhix/dbase:$RVERSION
 
-sudo podman build --pull --no-cache --squash-all --tag=fhix/dbase:$RVERSION --build-arg RVERSION=$RVERSION $WORKSPACE/dbase
+sudo podman build \
+--pull \
+--no-cache \
+--squash-all \
+--tag=fhix/dbase:$RVERSION \
+--build-arg RVERSION=$RVERSION \
+--build-arg CRAN_CHECKPOINT=$CRAN_CHECKPOINT
+$WORKSPACE/dbase
