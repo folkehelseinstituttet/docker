09568f8d911b5fc32d7127982b522ec0f4907618
Richard White <hello@rwhite.no>
2021-01-22 07:39:11 +0100

    Update CRAN_CHECKPOINT.sh

--- bin/CRAN_CHECKPOINT.sh
+++ bin/CRAN_CHECKPOINT.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Jan 21, 2021 1:00 AM GMT+1 for Ubuntu 20.04
-export CRAN_CHECKPOINT=https://packagemanager.rstudio.com/all/__linux__/focal/926967 
+# currently we are snapshotted to Jan 12, 2021 1:00 AM GMT+1 for Ubuntu 20.04 (Focal)
+# This is very important, because R package pool 0.1.6 (released 2021-01-14) does not
+# work with dbplyr, which means the shiny server can't access the database
+export CRAN_CHECKPOINT=https://packagemanager.rstudio.com/all/__linux__/focal/766976
 
 # get checkpoint IDs here:
 # https://packagemanager.rstudio.com/client/#/repos/1/overview
