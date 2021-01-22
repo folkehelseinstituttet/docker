ee756ad9db4c385289594a1a44b11e4c1fbd9fdc
Richard White <hello@rwhite.no>
2021-01-22 03:18:49 +0100

    fixing specification of CRAN_CHECKPOINT

--- bin/CRAN_CHECKPOINT.sh
+++ bin/CRAN_CHECKPOINT.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export CRAN_CHECKPOINT=16803
+export CRAN_CHECKPOINT=https://packagemanager.rstudio.com/all/__linux__/focal/16803
 
-# get checkpoint IDs here: 
+# get checkpoint IDs here:
 # https://packagemanager.rstudio.com/client/#/repos/1/overview
 # click on any date
-# below the date picker, see the generated URl - press "change" and choose FOCAL20 from the dropdown 
+# below the date picker, see the generated URl - press "change" and choose FOCAL20 from the dropdown
 # grab the number from the link
 # place it into this file as the variable CRAN_CHECKPOINT
