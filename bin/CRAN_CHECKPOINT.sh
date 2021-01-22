019ef924b3505420ec3cd90ba79851f94847a11a
Richard White <hello@rwhite.no>
2021-01-22 04:11:30 +0100

    Update CRAN_CHECKPOINT.sh

--- bin/CRAN_CHECKPOINT.sh
+++ bin/CRAN_CHECKPOINT.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export CRAN_CHECKPOINT=https://packagemanager.rstudio.com/all/__linux__/focal/16803
+# Jan 21, 2021 1:00 AM GMT+1 for Ubuntu 20.04
+export CRAN_CHECKPOINT=https://packagemanager.rstudio.com/all/__linux__/focal/926967 
 
 # get checkpoint IDs here:
 # https://packagemanager.rstudio.com/client/#/repos/1/overview
