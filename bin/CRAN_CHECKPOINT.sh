21247c79d36c763ffd43d72e7b1aa83da78d0790
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-06-10 05:13:13 +0200

    upgrade to R4.1.0

--- bin/CRAN_CHECKPOINT.sh
+++ bin/CRAN_CHECKPOINT.sh
@@ -5,6 +5,9 @@
 # work with dbplyr, which means the shiny server can't access the database
 export CRAN_CHECKPOINT=https://packagemanager.rstudio.com/all/__linux__/focal/766976
 
+# setting snapshot to 2021-06-01 (decision taken on 2021-06-10 while upgrading to R 4.1.0)
+export CRAN_CHECKPOINT https://packagemanager.rstudio.com/all/__linux__/focal/3255292
+
 # get checkpoint IDs here:
 # https://packagemanager.rstudio.com/client/#/repos/1/overview
 # click on any date
--- bin/R_VERSION.sh
+++ bin/R_VERSION.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 export RVERSION=4.0.2
+
+# 2021-06-10 upgrade R version
+export RVERSION=4.1.0
