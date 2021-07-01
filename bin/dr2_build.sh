659f2bdeb0a3038b5164a7824ff722abd767ab61
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-07-01 12:51:09 +0200

    remove pull

--- bin/dr2_build.sh
+++ bin/dr2_build.sh
@@ -4,5 +4,5 @@ source $WORKSPACE/bin/env.sh
 
 sudo podman rmi fhix/dr:$R_VERSION
 
-sudo podman build --pull --no-cache --tag=fhix/dr2:$R_VERSION --build-arg R_VERSION=$R_VERSION $WORKSPACE/dr2
+sudo podman build --no-cache --tag=fhix/dr2:$R_VERSION --build-arg R_VERSION=$R_VERSION $WORKSPACE/dr2
 sudo podman tag fhix/dr2:$R_VERSION fhix/dr:$R_VERSION
